d021c008d11374ffbfc51db34d4022d37ae9e318
CEM YILDIZ <43467707+xtrrdnryprgrmmr@users.noreply.github.com>
2022-07-16 15:31:36 +0300

    Update MailModel.java

--- MobileEmulatorSimulator/src/main/java/Model/MailModel.java
+++ MobileEmulatorSimulator/src/main/java/Model/MailModel.java
@@ -2,68 +2,74 @@ package Model;
 
 import Operation.Constants;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Properties;
-import java.util.Set;
+
 
 public class MailModel extends BaseModel {
-   public By senderTo = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]");
+    public By senderTo = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]");
     public By lastSenderTo = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.TextView[1]");
     public By senderSubject = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[3]");
     public By lastSenderSubject = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.TextView");
     public By senderContent = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[4]");
+    public By lastSenderContentTo = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View[2]");
     public By lastSenderContent = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.TextView");
-     public By lastReceivedMail = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]");
+    public By lastReceivedMail = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.ViewGroup[1]");
     public By compose = By.id("com.google.android.gm:id/compose_button");
     public By options = By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]");
     public By send = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[9]/android.widget.LinearLayout");
+
     public MailModel(AppiumDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
-    public void clickOptions() throws IOException {
+
+    public void clickOptions() {
         clickElement(options);
     }
-    public void clickSend() {
 
+    public void clickSend() {
         clickElement(send);
     }
-    public void clickLastReceivedMail() {
 
+    public void clickLastReceivedMail() {
         clickElement(lastReceivedMail);
     }
-    public void verifyMailFromSender(){
-        System.out.println("senderMailSubject : "+ getText(lastSenderSubject));
-        System.out.println("senderTo : "+ getText(lastSenderTo));
-        System.out.println("senderContent : "+getText(lastSenderContent));
-        assertElementContainsText(Constants.senderMailSubject,lastSenderSubject);
 
-        assertElementText(Constants.senderMail,lastSenderTo);
-
-        assertElementText(Constants.senderMailContent,lastSenderContent);
-
-        System.out.println("[TEST] Mobile Test Automation Part send successfully ");
-    }
-    public void verifyMailFromReceiver(){
-        System.out.println("senderMailSubject : "+ getText(lastSenderSubject));
-        System.out.println("senderTo : "+ getText(lastSenderTo));
-        System.out.println("senderContent : "+getText(lastSenderContent));
-        assertElementContainsText(Constants.receiverMailSubject,lastSenderSubject);
-        assertElementText(Constants.receiverMail,lastSenderTo);
-        assertElementText(Constants.receiverMailContent,lastSenderContent);
-        System.out.println("[TEST] Mobile Test Automation Part send successfully ");
+    public void verifyMailIsSend() {
+        System.out.println("sendedMailSubject : " + getText(lastSenderSubject));
+        System.out.println("sendedTo : " + getText(lastSenderTo));
+        System.out.println("sendedContent : " + getText(lastSenderContentTo));
+        assertElementContainsText(Constants.senderMailSubject, lastSenderSubject);
+        assertElementContainsText(Constants.senderMail, lastSenderTo);
+        assertElementContainsText(Constants.senderMailContent, lastSenderContentTo);
+        System.out.println("[TEST] Mobile Test Automation Part Mail sended successfully");
     }
 
-    public void sendMail(String sender,String receiver,String subject,String content,String passwd) {
+    public void verifyMailFromSender() {
+        System.out.println("senderMailSubject : " + getText(lastSenderSubject));
+        System.out.println("senderTo : " + getText(lastSenderTo));
+        System.out.println("senderContent : " + getText(lastSenderContent));
+        assertElementContainsText(Constants.senderMailSubject, lastSenderSubject);
+        assertElementContainsText(Constants.senderMail, lastSenderTo);
+        assertElementContainsText(Constants.senderMailContent, lastSenderContent);
+        System.out.println("[TEST] Mobile Test Automation Part send successfully ");
+    }
+
+    public void verifyMailFromReceiver() {
+        System.out.println("senderMailSubject : " + getText(lastSenderSubject));
+        System.out.println("senderTo : " + getText(lastSenderTo));
+        System.out.println("senderContent : " + getText(lastSenderContent));
+        assertElementContainsText(Constants.receiverMailSubject, lastSenderSubject);
+        assertElementText(Constants.receiverMail, lastSenderTo);
+        assertElementText(Constants.receiverMailContent, lastSenderContent);
+        System.out.println("[TEST] Mobile Test Automation Part Reply send successfully");
+    }
+
+    public void sendMail(String sender, String receiver, String subject, String content, String passwd) {
         final String username = sender;
         final String password = passwd;
 
@@ -81,7 +87,6 @@ public class MailModel extends BaseModel {
                 });
 
         try {
-
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(sender));
             message.setRecipients(
